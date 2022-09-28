ee38bf668dc2431e2c022784e0d2e44c710111ef
Kenya <pascasciokenya97@gmail.com>
2022-09-27 19:48:16 -0700

    Added a message form

--- js/index.js
+++ js/index.js
@@ -16,3 +16,47 @@ for (let i = 0; i < skills.length; i++) {
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 }
+
+
+//message form 
+const messageForm = document.querySelector('[name = "leave_message"]');
+const submission = messageForm.addEventListener("submit", (event) =>
+{
+    const name = event.target.name.value; 
+    const email = event.target.email.value;
+    const message = event.target.message.value;
+
+    event.preventDefault();
+
+    console.log(name);
+    console.log(email);
+    console.log(message);
+   
+    
+    
+  
+   const messageSection = document.getElementById("messages");
+   const messageList = messageSection.querySelector("ul");
+   
+
+
+   const newMessage = document.createElement("li");
+   newMessage.innerHTML = ` <a href= "mailto:${email}"> ${name} </a> <span class = "msg"> ${message}</span> ` ;
+
+   let removeButton = document.createElement("button");
+   removeButton.innerHTML = "Remove";
+   removeButton.type = "button";
+
+   const removal = removeButton.addEventListener("click", (event) =>{
+       let entry = event.target.parentElement
+        entry.remove();
+    })
+
+    newMessage.appendChild(removeButton);
+    messageList.append(newMessage);
+    messageForm.reset();
+}
+ );
+
+
+
